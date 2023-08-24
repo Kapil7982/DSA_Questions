a9e1264dc89606adada9ae4ebb791270d1715393
Kapil7982 <rathor798299@gmail.com>
2023-08-24 15:26:56 +0530

    one hundred three

---
+++ DSA_Questions/common_dsa_questions/common_dsa_questions/FlowerManagement.java
@@ -0,0 +1,48 @@
+package common_dsa_questions;
+
+public class FlowerManagement {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,0,0,0,1};
+		
+		int n = arr.length;
+		
+		int m = 1;
+		
+		boolean result = flowerManagement(arr, n, m);
+        
+        if(result) 
+        {
+            System.out.println("Yes");
+        } 
+        else 
+        {
+            System.out.println("No");
+        }
+
+	}
+
+	private static boolean flowerManagement(int[] arr, int n, int m) {
+		
+        int count = 0;
+        
+        for(int i=0; i<n; i++) 
+        {
+            if(arr[i] == 0) 
+            {
+               
+                boolean prevEmpty = (i == 0) || (i > 0 && arr[i - 1] == 0);
+                boolean nextEmpty = (i == n - 1) || (i < n - 1 && arr[i + 1] == 0);
+                
+                if (prevEmpty && nextEmpty) 
+                {
+                    arr[i] = 1; 
+                    count++;
+                }
+            }
+	}
+        return count >= m;
+
+	}
+}
