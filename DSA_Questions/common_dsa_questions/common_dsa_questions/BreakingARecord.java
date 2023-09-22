904900f7c4dc0920e775625a3a7f88c13430131f
Kapil7982 <rathor798299@gmail.com>
2023-09-22 13:02:53 +0530

    one hundred twinty three

---
+++ DSA_Questions/common_dsa_questions/common_dsa_questions/BreakingARecord.java
@@ -0,0 +1,25 @@
+package common_dsa_questions;
+
+public class BreakingARecord {
+
+	public static void main(String[] args) {
+		
+		int n = 264;
+		
+		int m = 200;
+		
+		if(m>n)
+	    {
+			System.out.println("Broken");
+	    }
+	    else if(m<n)
+	    {
+	    	System.out.println("Not Yet");
+	    }
+	    else if (m==n)
+	    {
+	    	System.out.println("Wao");
+	    }
+	}
+
+}
