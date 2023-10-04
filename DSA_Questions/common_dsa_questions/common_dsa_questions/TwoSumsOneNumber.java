86b28b67b4ca180b98d693c0853cbf5e967f226f
Kapil7982 <rathor798299@gmail.com>
2023-10-04 14:40:50 +0530

    one hundred thirty three

---
+++ DSA_Questions/common_dsa_questions/common_dsa_questions/TwoSumsOneNumber.java
@@ -0,0 +1,23 @@
+package common_dsa_questions;
+
+public class TwoSumsOneNumber {
+
+	public static void main(String[] args) {
+		
+		int one = 1;
+		int two = 2;
+		int three = 3;
+		int four = 4;
+		int five = 5;
+		
+		if((one+two)>five && (three+four)>five)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+	}
+
+}
