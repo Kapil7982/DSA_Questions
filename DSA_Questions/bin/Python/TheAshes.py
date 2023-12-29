b0f7dbdc2ddaa525eb7c395fd5addca50b6656b9
Kapil <103938868+Kapil7982@users.noreply.github.com>
2023-12-29 11:23:40 +0530

    TheAshes

---
+++ DSA_Questions/bin/Python/TheAshes.py
@@ -0,0 +1,10 @@
+def theAshes(Australia, England):
+    if Australia > England:
+        print("Australia")
+    elif England > Australia:
+        print("England")
+    elif England == Australia:
+        print("Tie")
+
+
+theAshes(46, 67)
