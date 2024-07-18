e4e7008c69e82994a35b0a81aa4885f4250cb137
Kapil7982 <rathor798299@gmail.com>
2024-07-18 14:57:22 +0530

    Three max and three min

---
+++ DSA_Questions/common_dsa_questions/common_dsa_questions/ThreeMaxThreeMin.java
@@ -0,0 +1,73 @@
+package common_dsa_questions;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class ThreeMaxThreeMin {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 3, 4, 2, 1, 6, 5};
+		
+		int n = arr.length;
+		
+		if(n>5) 
+        {
+            LinkedHashSet<Integer> hset = new LinkedHashSet<>();
+            
+            Arrays.sort(arr);
+            
+            for(int i=0; i<n; i++) 
+            {
+                hset.add(arr[i]);
+            }
+            
+            Iterator iterator = hset.iterator();
+            
+            for(int j=0; iterator.hasNext(); j++) 
+            {
+                int value = (int) iterator.next();
+                
+                if(hset.size()<6)
+                {
+                    if(j<=2) 
+                    {
+                        System.out.print(value + " ");
+                    }
+                    if(j==2) 
+                    {
+                        System.out.println();
+                    }
+                    if(j>=2) 
+                    {
+                        System.out.print(value + " ");
+                    }
+
+                }
+                else 
+                {
+                    if(j<=2) 
+                    {
+                        System.out.print(value + " ");
+                    } 
+                    else if (j>= hset.size() - 3) 
+                    {
+                        System.out.print(value + " ");
+                    }
+
+                    if(j==2) 
+                    {
+                        System.out.println();
+                    }
+                }
+            }
+        }
+        else 
+        {
+            System.out.println("Not Possible");
+            System.out.println("Not Possible");
+        }
+	}
+
+}
