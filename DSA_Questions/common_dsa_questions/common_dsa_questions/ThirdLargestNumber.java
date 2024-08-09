48e6ec552af0d4d13c217b5966d8c7a3f9d4f667
Kapil7982 <rathor798299@gmail.com>
2024-08-09 15:13:34 +0530

    third largest number

--- DSA_Questions/common_dsa_questions/common_dsa_questions/ThirdLargestNumber.java
+++ DSA_Questions/common_dsa_questions/common_dsa_questions/ThirdLargestNumber.java
@@ -1,3 +1,31 @@
-class ThirdLargestNumber{
-  
+package common_dsa_questions;
+
+
+import java.util.Arrays;
+
+public class ThirdLargestNumber{
+
+    public static int findThirdLargest(int[] arr) 
+    {
+        if (arr.length < 3) 
+        {
+            System.out.println("Array should have at least 3 elements.");
+            return -1;
+        }
+
+        Arrays.sort(arr);
+        return arr[arr.length - 3];
+    }
+
+    public static void main(String[] args) 
+    {
+        int[] arr = {10, 4, 3, 50, 23, 90, 100};
+        int thirdLargest = findThirdLargest(arr);
+        
+        if(thirdLargest != -1) 
+        {
+            System.out.println(thirdLargest);
+        }
+    }
 }
+
