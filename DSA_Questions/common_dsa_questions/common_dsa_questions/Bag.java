6136c2654bb13bd7a682342e720d657fbd7796f8
Kapil7982 <rathor798299@gmail.com>
2024-08-18 15:44:56 +0530

    Bag

--- DSA_Questions/common_dsa_questions/common_dsa_questions/Bag.java
+++ DSA_Questions/common_dsa_questions/common_dsa_questions/Bag.java
@@ -1,7 +1,35 @@
+package common_dsa_questions;
+
 class Bag{
 
   public static void main(String[] args)
   {
-    
+	  String str = "aabbc";
+	 
+	  String bag = "abc";
+	  
+	  int[] bagFreq = new int[26];
+	  
+      for(char c : str.toCharArray()) 
+      {
+          bagFreq[c - 'a']++;
+      }
+      
+      boolean canForm = true;
+      for(char c : str.toCharArray()) 
+      {
+          if(bagFreq[c - 'a'] > 0) 
+          {
+              bagFreq[c - 'a']--;
+          } 
+          else 
+          {
+              canForm = false;
+              break;
+          }
+      }
+
+      System.out.println(canForm ? "Yes" : "No");
+	  
   }
 }
